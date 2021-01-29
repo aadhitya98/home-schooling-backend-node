ce339229eee033ca6382d21c115c4be225fdae89
Akashbabu007 <akash4babu@gmail.com>
2021-01-29 13:51:21 +0530

    Password hashing is working when resetting password

--- configuration/db.js
+++ configuration/db.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
-const MONGOURL = "mongodb+srv://home-schooling:godislove158@cluster0.ncicr.mongodb.net/HomeSchooling?retryWrites=true&w=majority";
+const MONGOURL = "mongodb+srv://home-schooling:godislove158@cluster0.ncicr.mongodb.net/HomeSchooling?w=majority&retryWrites=true";
 
-const MongoServer = async () => {
-  try {
-    await mongoose.connect(MONGOURL, {
-      useNewUrlParser: true
-    });
-    console.log("Connected to DB !!");
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
+const MongoServer = async() => {
+    try {
+        await mongoose.connect(MONGOURL, {
+            useNewUrlParser: true
+        });
+        console.log("Connected to DB !!");
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
 };
 
 module.exports = MongoServer;
