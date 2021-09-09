00e01ec669ad262297133eab9c7649c6fe37f55e
Akash Babu <akash4babu@gmail.com>
2021-09-09 11:48:56 +0530

    Change password for teacher done

--- routes/crudteacher.js
+++ routes/crudteacher.js
@@ -15,67 +15,89 @@ MongoClient.connect(url, function(err, db) {
     router.post('/upcomingclasses', function(req, res) {
         dbo.collection('ScheduleData').find({ 'User': req.body.user }).toArray((err, cus) => {
             cus.forEach(item => {
-                console.log('item',req.body);
-                if(req.body.teacherName == item.TeacherName && req.body.subject == item.Subject) {
+                console.log('item', req.body);
+                if (req.body.teacherName == item.TeacherName && req.body.subject == item.Subject) {
                     upcomingclassdetails.push(item);
                 }
             })
             res.send(upcomingclassdetails);
             upcomingclassdetails = [];
         });
-        
+
         console.info('Scheduler api for upcoming classes for teacher ui is called');
     })
 
 
 });
 
-router.post('/createassignment',async function(req,res) {
-    const {classs,section,addassignment} = req.body;
-    console.log('classs',classs);
+router.post('/changepassword', async function(req, res) {
+    const { email, newPassword } = req.body;
+    // const salt = await bcrypt.genSalt(15);
+    // const encryptednewpassword = await bcrypt.hash(newPassword, salt)
+    try {
+        await User.updateOne({
+                email: email
+            }, { password: newPassword }
+
+        );
+        console.info('change password api in teacher UI is called');
+    } catch (err) {
+        console.log("Error", err.message);
+        res.status(500).send("Error in Storing the details to the DB");
+    }
+    return res.status(200).json({
+        message: "Password has been successfully changed"
+    })
+
+});
+
+router.post('/createassignment', async function(req, res) {
+    const { classs, section, addassignment } = req.body;
+    console.log('classs', classs);
     //Assignment.find({"teacheremail": teacheremail},(err,assignment)=>{
-        let assignmentteacher;
-        try {
-            let c = req.body.class
+    let assignmentteacher;
+    try {
+        let c = req.body.class
             // addassignment.forEach(async res => {
             //     console.log("res.teacheremail",res.teacheremail)
             //     assignmentteacher = await User.findOne({
             //         addassignment.teacheremail:res.teacheremail
             //     });
             // })
-            
-            // if (!assignmentteacher)
-            //     return res.status(400).json({
-            //         message: "Teacher Email Not Exists"
-            //     });
-                assignmentteacher = new Assignment({
-                    c,section,
-                    addassignment
-                });
-                dataStorage(c,section,addassignment);
-                return res.status(200).json({
-                    message: "Assignment is successfully added",
-                })
-            } catch(err){
-                console.log("Error", err.message);
-                res.status(500).send("Error in Storing Assignments to DB");
-            }
-    })
 
-    async function dataStorage(classs,section, addassignment) {
-        await Assignment.updateOne({
-            class: classs,
-            section: section,
-        }, { $addToSet: { addassignment: addassignment } }, { upsert: true });
-    }
-
-    router.post('/getassignment',async function(req,res) {
-        const {classs,section} = req.body;
-        Assignment.find({class: req.body.class, section: section}, (err,assignment) => {
-            res.send(assignment);
-            if( err) throw err;
+        // if (!assignmentteacher)
+        //     return res.status(400).json({
+        //         message: "Teacher Email Not Exists"
+        //     });
+        assignmentteacher = new Assignment({
+            c,
+            section,
+            addassignment
+        });
+        dataStorage(c, section, addassignment);
+        return res.status(200).json({
+            message: "Assignment is successfully added",
         })
-    });
+    } catch (err) {
+        console.log("Error", err.message);
+        res.status(500).send("Error in Storing Assignments to DB");
+    }
+})
+
+async function dataStorage(classs, section, addassignment) {
+    await Assignment.updateOne({
+        class: classs,
+        section: section,
+    }, { $addToSet: { addassignment: addassignment } }, { upsert: true });
+}
+
+router.post('/getassignment', async function(req, res) {
+    const { classs, section } = req.body;
+    Assignment.find({ class: req.body.class, section: section }, (err, assignment) => {
+        res.send(assignment);
+        if (err) throw err;
+    })
+});
 
 //)
 
